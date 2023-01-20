b4aba75a147d12de913e3aee8f4e0f14786154b1
neodain <neodain.net@gmail.com>
2023-01-20 15:15:37 +0900

    Articles deploy test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { getAllPosts, PostMeta } from "@/src/api";
+
 import Articles from "@/src/components/Articles";
 
 export default function Home({ posts }: { posts: PostMeta[] }) {
