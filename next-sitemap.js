2158812b19cae9506ee81624f82a3d0bae1339e0
neodain <neodain.net@gmail.com>
2023-01-21 09:00:25 +0900

    fix site-map

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: process.env.SITE_URL || "http://localhost:3000/",
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-  },
-};
