990684e4c8a38ea5a69cbf36eb0b176e8e9ff0e9
neodain <neodain.net@gmail.com>
2023-01-22 20:24:56 +0900

    fix logo-name

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -44,9 +44,32 @@ export default function Nav() {
           <LogoIcon />
         </div>
         <div className="brand-name">
-          <Link href="/">
+          <ul className="name-list">
+            <li className="name-1">
+              <Link href="/">N</Link>
+            </li>
+            <li className="name-2">
+              <Link href="/">e</Link>
+            </li>
+            <li className="name-3">
+              <Link href="/">o</Link>
+            </li>
+            <li className="name-4">
+              <Link href="/">d</Link>
+            </li>
+            <li className="name-5">
+              <Link href="/">a</Link>
+            </li>
+            <li className="name-6">
+              <Link href="/">i</Link>
+            </li>
+            <li className="name-7">
+              <Link href="/">n</Link>
+            </li>
+          </ul>
+          {/* <Link href="/">
             <img className="logo-img" src="images/neodain_04.png" alt="" />
-          </Link>
+          </Link> */}
         </div>
       </div>
 
